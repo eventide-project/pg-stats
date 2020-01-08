97c788f57426937cc21b8f10e09abb03378d8178
Scott Bellware <sbellware@gmail.com>
2020-01-08 15:53:35 -0600

    Statements

---
+++ scripts/statements.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/run.sh"
+
+echo
+echo "Statements"
+echo "= = ="
+echo "NOTE: Requires the pg_stats_statement extension"
+echo "https://www.postgresql.org/docs/current/pgstatstatements.html"
+echo
+
+run statements.sql
