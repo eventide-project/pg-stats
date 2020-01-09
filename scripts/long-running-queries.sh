4b1412df4852895787b0dd5ae7be9db624ee06c7
Scott Bellware <sbellware@gmail.com>
2020-01-08 21:20:44 -0600

    Long running queries

---
+++ scripts/long-running-queries.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/run.sh"
+
+echo
+echo "Long Running Queries"
+echo "= = ="
+echo
+
+run long-running-queries.sql
