06c5b6ff86730546de7d1f7ec71322eb426ac37f
Scott Bellware <sbellware@gmail.com>
2020-01-08 22:48:53 -0600

    Slow statements, rather than statements

--- scripts/slow-statements.sh
+++ scripts/slow-statements.sh
@@ -5,10 +5,10 @@ set -e
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/run.sh"
 
 echo
-echo "Statements"
+echo "Slow Statements"
 echo "= = ="
 echo "NOTE: Requires the pg_stats_statement extension"
 echo "https://www.postgresql.org/docs/current/pgstatstatements.html"
 echo
 
-run statements.sql
+run slow-statements.sql
